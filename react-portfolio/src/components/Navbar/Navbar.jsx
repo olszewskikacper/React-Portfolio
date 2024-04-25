4eb3f781a74dbf6f301c0279bf9120272be4154b
Kacper Olszewski <olszews.kacper@gmail.com>
2024-04-25 22:57:22 +0200

    first commit

---
+++ react-portfolio/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,33 @@
+import React, {useState} from 'react';
+
+import styles from "./Navbar.module.css";
+//import {getImageUrl} from "../../../utils";
+
+const Navbar = () => {
+  return (
+    <nav className={styles.navbar}>
+        <a className={styles.title} href='/'>Portfolio</a>
+        <div className={styles.menu}>
+            <img className={styles.menuBtn} src={getImageUrl("assets/menuIcon.png")}></img>
+            <ul className={styles.menuItems}>
+                <li>
+                    <a href="#about">About</a>
+                </li>
+
+                <li>
+                    <a href="#experience">Experience</a>
+                </li>
+
+                <li>
+                    <a href="#projects">Projects</a>
+                </li>
+                <li>
+                    <a href="#contact">Contact</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
